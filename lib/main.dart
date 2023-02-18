3abaf72999b1208f4eeda80ef03a3d48b5fa81bc
Umer <umerasrar17676@gmail.com>
2023-02-18 08:28:32 +0500

    Search functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/media_library.dart';
 import 'package:movie_app/watch.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'HomeScreen.dart';
 
 void main() {
@@ -15,7 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyHomePage(title: 'Movie App',),
+      home: MyHomePage(
+        title: 'Movie App',
+      ),
     );
   }
 }
@@ -46,10 +49,92 @@ class _MyHomePageState extends State<MyHomePage> {
     const mediaLibrary()
   ];
 
+  bool _showSearchBar = false;
+  List _movies = [];
+
+  Future<List> _searchMovies(String query) async {
+    String apiKey = 'ba772d49635405ae1bcb76668e176747';
+    String url =
+        'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$query';
+
+    var response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      //print(data['results']);
+      return data['results'];
+    } else {
+      throw Exception('Failed to load search results');
+    }
+  }
+
+  Widget _buildMovieList() {
+    return ListView.builder(
+      itemCount: _movies.length,
+      itemBuilder: (BuildContext context, int index) {
+        var movie = _movies[index];
+        return ListTile(
+          leading: movie['poster_path'] == null
+              ? Image.network('https://static.thenounproject.com/png/3674270-200.png' ,
+            height: 150,
+            width: 120,
+            fit: BoxFit.fill,)
+              : Image.network(
+            height: 150,
+            width: 120,
+            'https://image.tmdb.org/t/p/w92${movie['poster_path']}',
+            fit: BoxFit.fill,
+          ),
+          title: Text(movie['title']),
+          subtitle: Text(movie['release_date'].toString()),
+          trailing: const Icon(Icons.more_horiz),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
+      appBar: AppBar(
+        elevation: 1.0,
+        backgroundColor: Colors.white,
+        title: !_showSearchBar
+            ? Text(
+                _pageTitles[_selectedIndex],
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              )
+            : TextField(
+                autofocus: true,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search , color: Colors.black,),
+                  hintText: 'TV shows, movies and more',
+                ),
+                onChanged: (value) async {
+                  var results = await _searchMovies(value);
+                  setState(() {
+                    _movies = results;
+                  });
+                },
+              ),
+        actions: [
+          IconButton(
+            icon: _showSearchBar
+                ? const Icon(Icons.close, color: Colors.black)
+                : const Icon(Icons.search, color: Colors.black),
+            onPressed: () {
+              setState(() {
+                _showSearchBar = !_showSearchBar;
+                _movies = [];
+              });
+            },
+          ),
+        ],
+      ),
+
+      /*appBar: AppBar(
             elevation: 1.0,
             backgroundColor: Colors.white,
             actions: <Widget>[
@@ -61,43 +146,47 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Text(_pageTitles[_selectedIndex], style: const TextStyle(
               color: Colors.black,
             ),
-          )),
-          body: pages[_selectedIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            elevation: 50.0,
-            backgroundColor: Colors.black87,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white70,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                backgroundColor: Colors.white,
-                icon: Icon(Icons.dashboard),
-                label: 'Dashboard',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.play_circle),
-                label: 'watch',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.library_music),
-                label: 'Media Library',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.more_horiz),
-                label: 'More',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            //selectedItemColor: Colors.blue,
-            onTap: (int index) {
-              if (mounted) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              }
-            },
+          )),*/
+      body: !_showSearchBar
+          ? pages[_selectedIndex]
+          : _movies.isNotEmpty
+              ? _buildMovieList()
+              : Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        elevation: 50.0,
+        backgroundColor: Colors.black87,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
           ),
-        );
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle),
+            label: 'watch',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Media Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        //selectedItemColor: Colors.blue,
+        onTap: (int index) {
+          if (mounted) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          }
+        },
+      ),
+    );
   }
 }
--- lib/movieDetailScreen.dart
+++ lib/movieDetailScreen.dart
@@ -16,11 +16,16 @@ class _MovieDetailState extends State<MovieDetail> {
   late Map<String, dynamic> _movieDetails;
   bool _isLoading = true;
 
+  /*_MovieDetailState() {
+    images1 ??= [];
+  }*/
+
   @override
   void initState() {
     super.initState();
+    getMovieImages1(widget.id);
     _fetchMovieDetails();
-    getMovieImages(widget.id);
+    //getMovieImages(widget.id);
   }
 
   void _fetchMovieDetails() async {
@@ -47,8 +52,23 @@ class _MovieDetailState extends State<MovieDetail> {
     return genres;
   }
 
-
   late List<dynamic> _images;
+  List images1 = [];
+  Future<List> getMovieImages1(int movieId) async {
+    String apiKey = 'ba772d49635405ae1bcb76668e176747';
+    Uri apiUrl = Uri.parse(
+        'https://api.themoviedb.org/3/movie/$movieId/images?api_key=$apiKey');
+
+    var response = await http.get(apiUrl);
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body)['posters'];
+      images1 = data.map((e) => e['file_path']).toList();
+      return images1;
+    } else {
+      throw Exception('Failed to load images');
+    }
+  }
+
 
   void getMovieImages(int movieId) async {
     String apiKey = 'ba772d49635405ae1bcb76668e176747';
@@ -78,43 +98,46 @@ class _MovieDetailState extends State<MovieDetail> {
                   children: [
                     Stack(
                       children: [
-
                         Container(
                           height: 500.0,
                           width: double.infinity,
                           decoration: BoxDecoration(
                               image: DecorationImage(
                                   image: NetworkImage(
-                                      "https://image.tmdb.org/t/p/w500${_images[0]['file_path']}"),
+                                      "https://image.tmdb.org/t/p/w500${images1[0]}"),
                                   fit: BoxFit.fill)),
                         ),
                         Positioned(
-                            top: 0.0,
-                            child: Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Row(
-                                  children: [
-                                    IconButton(
-                                      icon: const Icon(Icons.arrow_back,
-                                        color: Colors.white, size: 30.0,),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
+                          top: 0.0,
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.white,
+                                      size: 30.0,
                                     ),
-                                    const SizedBox(width: 10.0),
-                                    const Text(
-                                      'Watch',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 25.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                  const SizedBox(width: 10.0),
+                                  const Text(
+                                    'Watch',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
-                            ),),
+                            ),
+                          ),
+                        ),
                         Positioned(
                           top: 280.0,
                           left: 10.0,
@@ -130,27 +153,29 @@ class _MovieDetailState extends State<MovieDetail> {
                               children: [
                                 Expanded(
                                     child: Padding(
-                                      padding: const EdgeInsets.all(15.0),
-                                      child: Container(
-                                        height: double.infinity,
-                                        width: double.infinity,
-                                        child: ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                              primary: Colors.lightBlueAccent,
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(20),
-                                              )
-                                          ),
-                                          onPressed: () {
-                                            // done functionality
-                                          },
-                                          child: const Text('Get Tickets', style: TextStyle(fontSize: 30.0),),
-                                          /*color: Colors.grey,
-                                  textColor: Colors.white,*/
-                                        ),
+                                  padding: const EdgeInsets.all(15.0),
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.lightBlueAccent,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          )),
+                                      onPressed: () {
+                                        // done functionality
+                                      },
+                                      child: const Text(
+                                        'Get Tickets',
+                                        style: TextStyle(fontSize: 30.0),
                                       ),
-                                    )),
-
+                                      /*color: Colors.grey,
+                                  textColor: Colors.white,*/
+                                    ),
+                                  ),
+                                )),
                                 const SizedBox(width: 10.0),
                                 Expanded(
                                     child: Padding(
@@ -160,25 +185,42 @@ class _MovieDetailState extends State<MovieDetail> {
                                     width: double.infinity,
                                     child: ElevatedButton(
                                       style: ElevatedButton.styleFrom(
-                                        primary: Colors.transparent,
-                                        side: const BorderSide(color: Colors.blue, width: 2, style: BorderStyle.solid),
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(20),
-                                          )
-                                      ),
+                                          primary: Colors.transparent,
+                                          side: const BorderSide(
+                                              color: Colors.blue,
+                                              width: 2,
+                                              style: BorderStyle.solid),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          )),
                                       onPressed: () {
-                                        Navigator.push(context,
-                                            MaterialPageRoute(builder: (context) =>
-                                                VideoPlayer(
-                                                id: widget.id,
-                                                )));
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    VideoPlayer(
+                                                      id: widget.id,
+                                                    )));
                                       },
                                       child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
                                         children: const [
-                                          Icon(Icons.play_arrow, color: Colors.white, size: 30.0,),
-                                          SizedBox(width: 10.0,),
-                                          Text('Watch Trailer', style: TextStyle(fontSize: 30.0, color: Colors.white),),
+                                          Icon(
+                                            Icons.play_arrow,
+                                            color: Colors.white,
+                                            size: 30.0,
+                                          ),
+                                          SizedBox(
+                                            width: 10.0,
+                                          ),
+                                          Text(
+                                            'Watch Trailer',
+                                            style: TextStyle(
+                                                fontSize: 30.0,
+                                                color: Colors.white),
+                                          ),
                                         ],
                                       ),
                                       /* color: Colors.blue,
@@ -199,10 +241,10 @@ class _MovieDetailState extends State<MovieDetail> {
                         style: TextStyle(fontSize: 25.0),
                       ),
                     ),
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: Text(
-                          _buildGenresString(),
+                        _buildGenresString(),
                         style: const TextStyle(fontSize: 20.0),
                       ),
                     ),
@@ -220,11 +262,13 @@ class _MovieDetailState extends State<MovieDetail> {
                         style: TextStyle(fontSize: 25.0),
                       ),
                     ),
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: Text(
                         _movieDetails['overview'],
-                        style: const TextStyle(fontSize: 18.0, ),
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                        ),
                         textAlign: TextAlign.justify,
                       ),
                     ),
